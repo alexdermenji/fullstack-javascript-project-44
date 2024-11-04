f2e13914d3717542de3429d7d2566d48a033f15e
Alexandr Dermenji <alexandr.dermenji@travisperkins.co.uk>
2024-11-04 14:36:37 +0000

    fix test

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 
 export default function greeting() {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
   return userName;
 }
